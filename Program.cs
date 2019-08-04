33821eeeb65581830a770f4debc13af4b4260e02
JLQmiller <894447836@qq.com>
2019-08-05 06:02:20 +0800

    start c-sharp

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FirstCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = FeetToInches(30);
+            Console.WriteLine(x);
+            
+        }
+
+        static int FeetToInches(int feet) {
+            int inches = feet * 12;
+            return inches;
+        }
+    }
+}
